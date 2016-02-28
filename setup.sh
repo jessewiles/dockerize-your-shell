b205c47dddaeba5a976eb2ce21a74cb7eab0b645
Jesse Wiles <jesse.wiles@gmail.com>
2016-02-27 23:37:57 -0600

    Point to other branch for windows for now.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# curl -sL https://raw.githubusercontent.com/steveortiz/dockerize-your-shell/master/setup.sh | bash -
+# curl -sL https://raw.githubusercontent.com/jessewiles/dockerize-your-shell/windows/setup.sh | bash -
 
 # add environment variables and script .bashrc
 echo "# Dockerize your Shell" >> ~/.bashrc
@@ -10,7 +10,7 @@ echo "DOCKERFILE_PATH=~/.dockerize-your-shell" >> ~/.bashrc
 echo "source ~/.dockerize-your-shell/dockerize-your-shell.sh" >> ~/.bashrc
 
 mkdir -p ~/.dockerize-your-shell
-GIT_URL=https://raw.githubusercontent.com/steveortiz/dockerize-your-shell/master
+GIT_URL=https://raw.githubusercontent.com/jessewiles/dockerize-your-shell/windows
 curl -sL $GIT_URL"/dockerize-your-shell.sh" -o ~/.dockerize-your-shell/dockerize-your-shell.sh
 # TODO: check for a dockerfile before possibly writing over it!
 curl -sL $GIT_URL"/example/Dockerfile" -o ~/.dockerize-your-shell/Dockerfile
